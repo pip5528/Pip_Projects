076e4d4820edfaa2e704c3dc0ed6416b624fc271
pip5528 <superpip5528@gmail.com>
2026-08-26 13:05:19 -0600

    --allow-empty

--- f2c.c
+++ f2c.c
@@ -7,7 +7,7 @@ int main(void) {
     printf("Enter a temperature in Fahrenheit:\n");
     scanf("%f", &fahr);
     
-    const float celsius = (fahr - 32.0) * 5.0/9.0f;
+    float celsius = (fahr - 32.0) * 5.0 / 9.0f;
     printf("%f", celsius);
 
 
